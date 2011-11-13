6a8d24c418e1cfd22641c02aa6f5b8bdbbd12f1b
dody <dodyrw@gmail.com>
2011-11-14 05:49:15 +0700

    relocate to new folder

---
+++ ceria/ceria_detail.php
@@ -0,0 +1,24 @@
+<?php
+  $t_ceria['id'] = 'ceria';
+  $t_ceria['name'] = 'ceria';
+  $t_ceria['author'] = 'Dody Rachmat Wicaksono';
+  $t_ceria['web'] = 'http://www.indexupremium.com';
+  $t_ceria['preview'] = '';
+  $t_ceria['position'] = array(
+    'topmenu1',
+    'topmenu2',
+    'menu1',
+    'menu2',
+    'breadcrumb',
+    'header right',
+    'main',
+    'main left',
+    'main right',
+    'sidebar',
+    'footer1',
+    'footer2',
+    'footer3',
+    'footer4',
+    'footer'
+  );
+?>
